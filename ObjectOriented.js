ca513ab2108e9a724b04ede61976bc2732fe3461
Jennifer Hanson <jenniferhanson.sd@gmail.com>
2023-01-19 17:33:41 -0800

    tested code with menu options for create, view, and delete

--- ObjectOriented.js
+++ ObjectOriented.js
@@ -1,4 +1,14 @@
 var test = [];
+var suppliesList = [];
+function deleteFromArray(index, arr){
+    arr2 = []
+    for(let i = 0; i < arr.length; i++){
+        if (i != index){
+          arr2.push(arr[i])
+      }
+    }
+    return arr2;
+}
 
 class ArtSupplies {                 // a list of art supply properties
     constructor(medium, brand, type, count){
@@ -9,7 +19,6 @@ class ArtSupplies {                 // a list of art supply properties
         }
     // method to print out the properties of our art supplies
     describe() {        
-        console.log();
         console.log("Medium: " + this.medium);
         console.log("Brand: " + this.brand);
         console.log("Type: " + this.type);
@@ -38,47 +47,68 @@ class Menu{
     }
     start(){               // begins the iteration through the menu
         let selection = this.showMainMenuOptions();
-        while (selection != 0);
+        while (selection != 0) {
             switch (selection) {
                 case "1":
-                    this.createSupplyEntry;
+                    this.createSupplyEntry();
                     break;
                 case "2":
-                    this.viewSupplyEntry;
+                    this.viewSupplyEntry();
                     break;
                 case "3":
-                    this.deleteSupplyEntry;
+                    this.deleteSupplyEntry();
                     break;
                 case "4":
-                    this.displaySupplies;
+                    this.displaySupplyEntry();
                     break;
                 default:
                     selection = 0;  
             }
             selection = this.showMainMenuOptions();
-}
-showMainMenuOptions() {           // ends the menu prompt if 0 is selected
-    alert("Have a good day!");
-}
-showMediumOptions() {             // prompt to pick an option
-    return prompt(`
-    1) Create a supply entry
-    2) View supply entry
-    3) Delete supply entry
-    4) Display list of entered supplies
-    0) Exit menu
-    `);
-}
-displaySupplies() {
-    
-}
-deleteSupplyEntry() {
+        }
+        alert("Have a good day!");
+    }
+    showMainMenuOptions() {           // ends the menu prompt if 0 is selected
+                  // prompt to pick an option
+        return prompt(`
+        1) Create a supply entry
+        2) View supply entry
+        3) Delete supply entry
+        4) Display list of entered supplies
+        0) Exit menu
+        `);
 
-}
-createSupplyEntry() {
-    let medium = prompt(`Enter type of medium:`);
-    this.artSupplies.push(new Medium(medium));
-}
+    }
+    createSupplyEntry() {
+        let medium = prompt("medium");
+        let brand = prompt("brand");
+        let type = prompt("type");
+        let count = prompt("count");
+        let supplies = new ArtSupplies(medium, brand, type, count);
+        alert("createSupplyEntry");
+        suppliesList.push(supplies);
+    }
+    viewSupplyEntry(){
+        alert("Number of created supplies:" + suppliesList.length);
+    }
+    deleteSupplyEntry() {
+        alert("created supplies:" + suppliesList.length);
+        let deleteEntry = prompt("Enter a number from 0: " + (suppliesList.length-1));
+        suppliesList = deleteFromArray(deleteEntry,suppliesList);
+        alert("Deleted Supplies");
+    }
+    displaySupplyEntry() {
+        let s = "";
+            for(let i = 0; i < suppliesList.length; i++){
+                s += "medium = " + suppliesList[i].medium + "\n" +
+                     "brand = " + suppliesList[i].brand + "\n" +
+                     "type =" + suppliesList[i].type + "\n" +
+                     "count =" + suppliesList[i].count +
+                     "\n"
+            }
+         alert("Entered supplies:\n" + s);
+    }
+   
 }
 let myMenu = new Menu ();
 myMenu.start();
